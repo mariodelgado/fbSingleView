26f578f6c8a095cf2b36e35307e8a7a52be03cf2
Mario Delgado <mario.c.delgado@gmail.com>
2014-06-05 21:22:13 -0700

    Initial Commit

---
+++ fbSingleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbSingleView
+//
+//  Created by Mario C. Delgado Jr. on 6/5/14.
+//  Copyright (c) 2014 Mario C. Delgado Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
